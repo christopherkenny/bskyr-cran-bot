37bc4aa7a2f9e6a8e225f1bfaab4cea733f57bec
Christopher T. Kenny <ctk44@cornell.edu>
2025-05-25 12:06:29 -0400

    Update post.R

--- post.R
+++ post.R
@@ -17,7 +17,6 @@ if (!bs_has_pass()) {
   stop('bsky pass not found.')
 }
 auth <- bs_auth(bs_get_user(), bs_get_pass())
-auth$email
 
 # load available packages ----
 pkgs <- available.packages(filters = c('CRAN', 'duplicates')) |>
